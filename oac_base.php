7780952dd6aa396dae3270c93df7e765e767c54d
Christopher Villalobos <christopher.villalobos@gmail.com>
2011-01-19 10:17:48 -0500

    Added ENSO lookup and retrieval into the OACBase.
    Signed-off-by: Christopher Villalobos <christopher.villalobos@gmail.com>

--- oac_base.php
+++ oac_base.php
@@ -109,7 +109,104 @@ if( !class_exists( 'OACBase' ) ) {
 			</div>
 		<?php
 		}
+
+		static private function lookup_enso() {
+		$enso_array = array();
+		
+		// Retrieve the data from IRI	
+		$enso_uri = 'http://iri.columbia.edu/climate/ENSO/currentinfo/figure3.html';
+		$raw_html = wp_remote_fopen( $enso_uri );
+		
+		// Parse the table with the prediction data
+		$result = preg_match_all("/<tr><td>(?P<prediction_date>[JFMASOND]{3}\ [0-9]{4})<\/td><td>(?P<lanina>[0-9\. ]{1,4})%<\/td><td>(?P<neutral>[0-9\. ]{1,4})%<\/td><td>(?P<elnino>[0-9\. ]{1,4})%<\/td><\/tr>/", $raw_html, $parsed_data);
+		
+		// Return FALSE if any errors, otherwise return the $enso_array
+		if( $result ):
+			$mon_lookup = array();
+			$mon_lookup[] = substr(date( "M", strtotime('now')), 0, 1);
+			$mon_lookup[] = substr(date( "M", strtotime('+1 month')), 0, 1);
+			$mon_lookup[] = substr(date( "M", strtotime('+2 months')), 0, 1).' '.date("Y", strtotime('+2 months'));
+
+			$true_period = implode('', $mon_lookup);
+			$mon_index = array_search($true_period, $parsed_data['prediction_date']);
+			if( $mon_index === false ) return false;
+			// If we get a match back, then store this information in the database as well as update the current timestamp.
+			$enso_array['la_nina_prediction'] = floatval( $parsed_data['lanina'][$mon_index] );
+			$enso_array['neutral_prediction'] = floatval( $parsed_data['neutral'][$mon_index] );
+			$enso_array['el_nino_prediction'] = floatval( $parsed_data['elnino'][$mon_index] );
+			
+			// Guess the current phase based on the maximum value of the above
+			$current_phase = array_search( max( $enso_array ), $enso_array );
+			switch( substr( $current_phase, 0, 1 ) ) {
+				case 'l': // La Nina Phase
+					$enso_array['current_phase'] = __( 'La Ni&#241;a' );
+					break;
+				case 'n': // Neutral Phase
+					$enso_array['current_phase'] = __( 'Neutral' );
+					break;
+				case 'e': // El Nino Phase
+					$enso_array['current_phase'] = __( 'El Ni&#241;o' );
+					break;
+				default:
+					$enso_array['current_phase'] = __( 'Unknown' );
+					break;
+			}
+		
+			// Find the current prediciton period ( localized )
+			$month_list    = 'JFMAMJJASONDJF';
+			$pred_month_index  = stripos( $month_list, substr( $parsed_data['prediction_date'], 0, 3 ) ) + 1;
+			$current_period = array();
+			for( $i=0; $i < 3; $i++ ) {
+				$current_period[] = date_i18n( 'M', strtotime( ( ( $pred_month_index + $i ) % 12 ).'/1/'.date( 'Y' ) ) );
+			}
+			$enso_array['current_period'] = implode( '-', $current_period );
+			
+			// Set the last_updated to the current time	
+			$enso_array['last_updated'] = strtotime( 'now' );
+			return $enso_array;
+		else:
+			// Email the site administrator(s) to let them know there is a problem
+			return false;
+		endif;
 	}
+
+	static public function get_current_enso_data() {
+		$new_data   = false; // Set to true if new data is retrieved
+		
+		// Should I do my checks here or not?
+		if( $enso_array = get_option( 'oac_current_enso_data', false ) ) {
+			// Check the timestamp (if there is one) for freshness (2 weeks)
+			if( strtotime( '+1 day', $enso_array['last_updated'] ) < strtotime( 'now' ) ) {
+				// If there isn't an error getting the ENSO data, save it otherwise, keep our old data (the administrator got an email anyways)
+				$new_enso_array = self::lookup_enso();
+				if( $new_enso_array != false ) {
+					$enso_array = $new_enso_array;
+					$new_data = true;
+				}
+			}
+		} else {
+			// Lookup the data, save it and move on
+			$enso_array = self::lookup_enso();
+			if( $enso_array != false ) {
+				$new_data = true;
+			} else {
+				$enso_array = array();
+			}
+		} //if ( get_option ... )
+		if( $new_data )
+			update_option( 'oac_current_enso_data', $enso_array );
+		
+		return $enso_array;
+
+	}
+
+	static public function get_current_enso_phase() {
+		$enso_array = self::get_current_enso_data();
+		return $enso_array['current_period'];
+	}
+
+	
+
 }
 
 // WordPress Hooks
