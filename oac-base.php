60beca30530b048757d145610bf1c79b5539d620
Christopher Villalobos <christopher.villalobos@gmail.com>
2011-02-15 10:47:45 -0500

    Fixing race conditions
    - Removed unneeded CSS
    - Removed class_exists wrapper
    - Fixed race conditions for admin
    
    Signed-off-by: Christopher Villalobos <christopher.villalobos@gmail.com>

--- oac-base.php
+++ oac-base.php
@@ -26,218 +26,214 @@
 
 if( is_admin() ) require_once( 'scoper/wp-scoper-admin.php' );
 
-if( !class_exists( 'OACBase' ) ) {
-	class OACBase {
-		static public $units = null;
+class OACBase {
+	static public $units = null;
 
-	 	/**
-		 * Initializes pChart and (g)RaphaelJs libraries
-		 * 
-		 * init() Sets up the registration of the raphaelJS / gRaphael
-		 * vector library. To use, choose the TYPE of chart you desire:
-		 * <code> 
-		 * wp_enqueue_script('grpie'); // Includes raphael-min.js, g.raphael-min.js and g.pie-min.hs files
-		 * </code>
-		 * 
-		 * @since 1.0
-		 */
-		static public function init() {
-			// Put the requires inside of the init to make sure it's all or nothing on initialization
-			
-			// WP_Scoper
-			require_once( 'scoper/wp-scoper.php' );
-			
-			// raphaeljs/gRapahel script setup.
-			wp_register_script( 'raphaeljs', plugins_url( 'js/raphael.js', __FILE__ ) );
-			wp_register_script( 'graphael',  plugins_url( 'js/graphael/g.raphael.js', __FILE__ ) );
-			wp_register_script( 'grpie',     plugins_url( 'js/graphael/g.pie.js', __FILE__ ), array('raphaeljs', 'graphael') );
-			wp_register_script( 'grbar',     plugins_url( 'js/graphael/g.bar.js', __FILE__), array('raphaeljs', 'graphael' ) );
-			wp_register_script( 'grline',    plugins_url( 'js/graphael/g.line.js', __FILE__), array('raphaeljs', 'graphael' ) );
-			wp_register_script( 'oaclib',    plugins_url( 'js/oaclib.js', __FILE__ ), array( 'jquery','graphael' ) );
-			wp_register_script( 'wp-scoper', plugins_url( 'js/wp-scoper-js.php', __FILE__ ), array( 'jquery' ) );
-			
-			// CSS styles registration
-			wp_register_style( 'jquery-ui', plugins_url( 'js/jquery-ui/themes/base/jquery-ui.css', __FILE__ ) );
-			wp_register_style( 'jqplot',    plugins_url( 'js/jqplot/jquery.jqplot.min.css', __FILE__ ) );
-			wp_register_style( 'oacbase',   plugins_url( 'css/oac-base.css', __FILE__ ) );
-			
-			self::$units = array('Metric'=>array(
-					'smalllen'=>array(
-						'abbr' => __('mm'),
-						'name' => __('Millimeter')
-					),
-					'len' => array(
-						'abbr' => __('m'),
-						'name' => __('Meter'),
-					),
-					'largelen' => array(
-						'abbr' => __('km'),
-						'name' => __('Kilometer')
-					),
-					'temp'=> array(
-						'abbr' => __('&#176;C'),
-						'name' => __('&#176;Celsius')
-					)
+	/**
+	 * Initializes pChart and (g)RaphaelJs libraries
+	 * 
+	 * init() Sets up the registration of the raphaelJS / gRaphael
+	 * vector library. To use, choose the TYPE of chart you desire:
+	 * <code> 
+	 * wp_enqueue_script('grpie'); // Includes raphael-min.js, g.raphael-min.js and g.pie-min.hs files
+	 * </code>
+	 * 
+	 * @since 1.0
+	 */
+	static public function init() {
+		// Put the requires inside of the init to make sure it's all or nothing on initialization
+		
+		// WP_Scoper
+		require_once( 'scoper/wp-scoper.php' );
+		
+		// raphaeljs/gRapahel script setup.
+		wp_register_script( 'raphaeljs', plugins_url( 'js/raphael.js', __FILE__ ) );
+		wp_register_script( 'graphael',  plugins_url( 'js/graphael/g.raphael.js', __FILE__ ) );
+		wp_register_script( 'grpie',     plugins_url( 'js/graphael/g.pie.js', __FILE__ ), array('raphaeljs', 'graphael') );
+		wp_register_script( 'grbar',     plugins_url( 'js/graphael/g.bar.js', __FILE__), array('raphaeljs', 'graphael' ) );
+		wp_register_script( 'grline',    plugins_url( 'js/graphael/g.line.js', __FILE__), array('raphaeljs', 'graphael' ) );
+		wp_register_script( 'oaclib',    plugins_url( 'js/oaclib.js', __FILE__ ), array( 'jquery','graphael' ) );
+		wp_register_script( 'wp-scoper', plugins_url( 'js/wp-scoper-js.php', __FILE__ ), array( 'jquery' ) );
+		
+		// CSS styles registration
+		wp_register_style( 'jquery-ui', plugins_url( 'js/jquery-ui/themes/base/jquery-ui.css', __FILE__ ) );
+		wp_register_style( 'oacbase',   plugins_url( 'css/oac-base.css', __FILE__ ) );
+		
+		self::$units = array('Metric'=>array(
+				'smalllen'=>array(
+					'abbr' => __('mm'),
+					'name' => __('Millimeter')
 				),
-				'US' => array(
-					'smalllen'=>array(
-						'abbr'   => __('in'),
-						'name'   => __('Inch'),
-						'plural' => __('Inches')
-					),
-					'len' => array(
-						'abbr'   => __('ft'),
-						'name'   => __('Foot'),
-						'plural' => __('Feet')
-					),
-					'largelen' => array(
-						'abbr' => __('mi'),
-						'name' => __('Mile')
-					),
-					'temp' => array(
-						'abbr' => __('&#176;F'),
-						'name' => __('&$176;Fahrenheit')
-					)
+				'len' => array(
+					'abbr' => __('m'),
+					'name' => __('Meter'),
+				),
+				'largelen' => array(
+					'abbr' => __('km'),
+					'name' => __('Kilometer')
+				),
+				'temp'=> array(
+					'abbr' => __('&#176;C'),
+					'name' => __('&#176;Celsius')
 				)
-			);
-		}
+			),
+			'US' => array(
+				'smalllen'=>array(
+					'abbr'   => __('in'),
+					'name'   => __('Inch'),
+					'plural' => __('Inches')
+				),
+				'len' => array(
+					'abbr'   => __('ft'),
+					'name'   => __('Foot'),
+					'plural' => __('Feet')
+				),
+				'largelen' => array(
+					'abbr' => __('mi'),
+					'name' => __('Mile')
+				),
+				'temp' => array(
+					'abbr' => __('&#176;F'),
+					'name' => __('&$176;Fahrenheit')
+				)
+			)
+		);
+	}
+	
+	static public function get_unit( $callee, $var ) {
+		$type = get_option( $callee.'_units', get_option( 'oac_base_units', 'Metric' ) );
+		return ( isset( self::$units[$type][$var] ) ) ? self::$units[$type][$var] : '';
+	}
+	
+	static private function lookup_enso() {
+		$enso_array = array();
 		
-		static public function get_unit( $callee, $var ) {
-			$type = get_option( $callee.'_units', get_option( 'oac_base_units', 'Metric' ) );
-			return ( isset( self::$units[$type][$var] ) ) ? self::$units[$type][$var] : '';
-		}
+		// Retrieve the data from IRI	
+		$enso_uri = 'http://iri.columbia.edu/climate/ENSO/currentinfo/figure3.html';
+		$raw_html = wp_remote_fopen( $enso_uri );
 		
-		static private function lookup_enso() {
-			$enso_array = array();
-			
-			// Retrieve the data from IRI	
-			$enso_uri = 'http://iri.columbia.edu/climate/ENSO/currentinfo/figure3.html';
-			$raw_html = wp_remote_fopen( $enso_uri );
-			
-			// Parse the table with the prediction data
-			$result = preg_match_all("/<tr><td>(?P<prediction_date>[JFMASOND]{3}\ [0-9]{4})<\/td><td>(?P<lanina>[0-9\. ]{1,4})%<\/td><td>(?P<neutral>[0-9\. ]{1,4})%<\/td><td>(?P<elnino>[0-9\. ]{1,4})%<\/td><\/tr>/", $raw_html, $parsed_data);
-			
-			// Return FALSE if any errors, otherwise return the $enso_array
-			if( $result ):
-				$mon_lookup = array();
-				$mon_lookup[] = substr(date( "M", strtotime('now')), 0, 1);
-				$mon_lookup[] = substr(date( "M", strtotime('+1 month')), 0, 1);
-				$mon_lookup[] = substr(date( "M", strtotime('+2 months')), 0, 1).' '.date("Y", strtotime('+2 months'));
+		// Parse the table with the prediction data
+		$result = preg_match_all("/<tr><td>(?P<prediction_date>[JFMASOND]{3}\ [0-9]{4})<\/td><td>(?P<lanina>[0-9\. ]{1,4})%<\/td><td>(?P<neutral>[0-9\. ]{1,4})%<\/td><td>(?P<elnino>[0-9\. ]{1,4})%<\/td><\/tr>/", $raw_html, $parsed_data);
+		
+		// Return FALSE if any errors, otherwise return the $enso_array
+		if( $result ):
+			$mon_lookup = array();
+			$mon_lookup[] = substr(date( "M", strtotime('now')), 0, 1);
+			$mon_lookup[] = substr(date( "M", strtotime('+1 month')), 0, 1);
+			$mon_lookup[] = substr(date( "M", strtotime('+2 months')), 0, 1).' '.date("Y", strtotime('+2 months'));
 
-				$true_period = implode('', $mon_lookup);
-				$mon_index = array_search($true_period, $parsed_data['prediction_date']);
-				if( $mon_index === false ) return false;
-				// If we get a match back, then store this information in the database as well as update the current timestamp.
-				$enso_array['la_nina_prediction'] = floatval( $parsed_data['lanina'][$mon_index] );
-				$enso_array['neutral_prediction'] = floatval( $parsed_data['neutral'][$mon_index] );
-				$enso_array['el_nino_prediction'] = floatval( $parsed_data['elnino'][$mon_index] );
-				
-				// Guess the current phase based on the maximum value of the above
-				$current_phase = array_search( max( $enso_array ), $enso_array );
-				switch( substr( $current_phase, 0, 1 ) ) {
-					case 'l': // La Nina Phase
-						$enso_array['current_phase'] = __( 'La Ni&#241;a' );
-						break;
-					case 'n': // Neutral Phase
-						$enso_array['current_phase'] = __( 'Neutral' );
-						break;
-					case 'e': // El Nino Phase
-						$enso_array['current_phase'] = __( 'El Ni&#241;o' );
-						break;
-					default:
-						$enso_array['current_phase'] = __( 'Unknown' );
-						break;
-				}
+			$true_period = implode('', $mon_lookup);
+			$mon_index = array_search($true_period, $parsed_data['prediction_date']);
+			if( $mon_index === false ) return false;
+			// If we get a match back, then store this information in the database as well as update the current timestamp.
+			$enso_array['la_nina_prediction'] = floatval( $parsed_data['lanina'][$mon_index] );
+			$enso_array['neutral_prediction'] = floatval( $parsed_data['neutral'][$mon_index] );
+			$enso_array['el_nino_prediction'] = floatval( $parsed_data['elnino'][$mon_index] );
 			
-				// Find the current prediciton period ( localized )
-				$month_list    = 'JFMAMJJASONDJF';
-				$pred_month_index  = stripos( $month_list, substr( $parsed_data['prediction_date'][$mon_index], 0, 3 ) ) + 1;
-				$current_period = array();
-				for( $i=0; $i < 3; $i++ ) {
-					$current_period[] = date_i18n( 'M', strtotime( ( ( $pred_month_index + $i ) % 12 ).'/1/'.date( 'Y' ) ) );
-				}
-				$enso_array['current_period'] = implode( '-', $current_period );
-				
-				// Set the last_updated to the current time	
-				$enso_array['last_updated'] = strtotime( 'now' );
-				return $enso_array;
-			else:
-				// Email the site administrator(s) to let them know there is a problem
-				return false;
-			endif;
-		}
-
-		static public function get_current_enso_data() {
-			$new_data   = false; // Set to true if new data is retrieved
+			// Guess the current phase based on the maximum value of the above
+			$current_phase = array_search( max( $enso_array ), $enso_array );
+			switch( substr( $current_phase, 0, 1 ) ) {
+				case 'l': // La Nina Phase
+					$enso_array['current_phase'] = __( 'La Ni&#241;a' );
+					break;
+				case 'n': // Neutral Phase
+					$enso_array['current_phase'] = __( 'Neutral' );
+					break;
+				case 'e': // El Nino Phase
+					$enso_array['current_phase'] = __( 'El Ni&#241;o' );
+					break;
+				default:
+					$enso_array['current_phase'] = __( 'Unknown' );
+					break;
+			}
+		
+			// Find the current prediciton period ( localized )
+			$month_list    = 'JFMAMJJASONDJF';
+			$pred_month_index  = stripos( $month_list, substr( $parsed_data['prediction_date'][$mon_index], 0, 3 ) ) + 1;
+			$current_period = array();
+			for( $i=0; $i < 3; $i++ ) {
+				$current_period[] = date_i18n( 'M', strtotime( ( ( $pred_month_index + $i ) % 12 ).'/1/'.date( 'Y' ) ) );
+			}
+			$enso_array['current_period'] = implode( '-', $current_period );
 			
-			// Should I do my checks here or not?
-			if( $enso_array = get_option( 'oac_current_enso_data', false ) ) {
-				// Check the timestamp (if there is one) for freshness (2 weeks)
-				if( strtotime( '+1 day', $enso_array['last_updated'] ) < strtotime( 'now' ) ) {
-					// If there isn't an error getting the ENSO data, save it otherwise, keep our old data (the administrator got an email anyways)
-					$new_enso_array = self::lookup_enso();
-					if( $new_enso_array != false ) {
-						$enso_array = $new_enso_array;
-						$new_data = true;
-					}
-				}
-			} else {
-				// Lookup the data, save it and move on
-				$enso_array = self::lookup_enso();
-				if( $enso_array != false ) {
-					$new_data = true;
-				} else {
-					$enso_array = array();
-				}
-			} //if ( get_option ... )
-			if( $new_data )
-				update_option( 'oac_current_enso_data', $enso_array );
-			
+			// Set the last_updated to the current time	
+			$enso_array['last_updated'] = strtotime( 'now' );
 			return $enso_array;
+		else:
+			// Email the site administrator(s) to let them know there is a problem
+			return false;
+		endif;
+	}
 
-		}
-
-		static public function get_current_enso_phase() {
-			$enso_array = self::get_current_enso_data();
-			return $enso_array['current_phase'];
-		}
-
-		static public function display_enso_selector( $phases_only = false ) {
-			$current_phase_id = substr( self::get_current_enso_phase(), 0, 1 );
-			$output = '<ul id="enso-select">';
-			$output .= '<li class="neutral oac-enso-1 highlight"><input type="radio" class="oac-input oac-radio" name="ensophase" value="1"'.(($current_phase_id == 'N') ? ' checked' : '').'>'.__( 'Neutral' ).'</li>';
-			$output .= '<li class="elnino oac-enso-2 highlight"> <input type="radio" class="oac-input oac-radio" name="ensophase" value="2"'.(($current_phase_id == 'E') ? ' checked' : '').'>'.__( 'El Ni&#241;o' ).'</li>';
-			$output .= '<li class="lanina oac-enso-3 highlight"> <input type="radio" class="oac-input oac-radio" name="ensophase" value="3"'.(($current_phase_id == 'L') ? ' checked' : '').'>'.__( 'La Ni&#241;a' ).'</li>';
-			if ( ! $phases_only )
-				$output .= '<li class="allYears oac-enso-4 highlight"><input type="radio" class="oac-input oac-radio" name="ensophase" value="4">'.__( 'All Years' ).'</li>';
-			$output .= '</ul>';
-			return $output;
-		}
-
-		static public function ie_conditionals() {
-		}
+	static public function get_current_enso_data() {
+		$new_data   = false; // Set to true if new data is retrieved
 		
-		static public function knsort( $array ) {
-			$keys = array_keys($array);
-		    natsort($keys);
-			
-			$new_array = array();
-		    foreach ($keys as $k)
-		    {
-		        $new_array[$k] = $array[$k];
-		    }
+		// Should I do my checks here or not?
+		if( $enso_array = get_option( 'oac_current_enso_data', false ) ) {
+			// Check the timestamp (if there is one) for freshness (2 weeks)
+			if( strtotime( '+1 day', $enso_array['last_updated'] ) < strtotime( 'now' ) ) {
+				// If there isn't an error getting the ENSO data, save it otherwise, keep our old data (the administrator got an email anyways)
+				$new_enso_array = self::lookup_enso();
+				if( $new_enso_array != false ) {
+					$enso_array = $new_enso_array;
+					$new_data = true;
+				}
+			}
+		} else {
+			// Lookup the data, save it and move on
+			$enso_array = self::lookup_enso();
+			if( $enso_array != false ) {
+				$new_data = true;
+			} else {
+				$enso_array = array();
+			}
+		} //if ( get_option ... )
+		if( $new_data )
+			update_option( 'oac_current_enso_data', $enso_array );
 		
-			return $new_array;
-		}
+		return $enso_array;
+
+	}
+
+	static public function get_current_enso_phase() {
+		$enso_array = self::get_current_enso_data();
+		return $enso_array['current_phase'];
+	}
+
+	static public function display_enso_selector( $phases_only = false ) {
+		$current_phase_id = substr( self::get_current_enso_phase(), 0, 1 );
+		$output = '<ul id="enso-select">';
+		$output .= '<li class="neutral oac-enso-1 highlight"><input type="radio" class="oac-input oac-radio" name="ensophase" value="1"'.(($current_phase_id == 'N') ? ' checked' : '').'>'.__( 'Neutral' ).'</li>';
+		$output .= '<li class="elnino oac-enso-2 highlight"> <input type="radio" class="oac-input oac-radio" name="ensophase" value="2"'.(($current_phase_id == 'E') ? ' checked' : '').'>'.__( 'El Ni&#241;o' ).'</li>';
+		$output .= '<li class="lanina oac-enso-3 highlight"> <input type="radio" class="oac-input oac-radio" name="ensophase" value="3"'.(($current_phase_id == 'L') ? ' checked' : '').'>'.__( 'La Ni&#241;a' ).'</li>';
+		if ( ! $phases_only )
+			$output .= '<li class="allYears oac-enso-4 highlight"><input type="radio" class="oac-input oac-radio" name="ensophase" value="4">'.__( 'All Years' ).'</li>';
+		$output .= '</ul>';
+		return $output;
+	}
+
+	static public function ie_conditionals() {
+	}
+	
+	static public function knsort( $array ) {
+		$keys = array_keys($array);
+	    natsort($keys);
 		
-		static public function knrsort($array) {
-		    return array_reverse( self::knsort($array), true );
-		}
+		$new_array = array();
+	    foreach ($keys as $k)
+	    {
+		$new_array[$k] = $array[$k];
+	    }
+	
+		return $new_array;
+	}
+	
+	static public function knrsort($array) {
+	    return array_reverse( self::knsort($array), true );
 	}
 }
 // For once, we are putting the handling code for the admin at the bottom. This is a special
 // exception to the norm.
-
 class OACBaseAdmin {
 	static public function init() {
 		OACBase::init();
@@ -327,10 +323,8 @@ class OACBaseAdmin {
 add_action( 'plugins_loaded', array( 'OACBase', 'init' ), 9 );
 register_activation_hook( __FILE__, array( 'OACBaseAdmin', 'activate' ) );
 register_deactivation_hook( __FILE__, array( 'OACBaseAdmin', 'deactivate' ) );
-if( is_admin() ) {
-	add_action( 'admin_init', array('OACBaseAdmin', 'init' ) );
-	add_action( 'admin_menu', array( 'OACBaseAdmin', 'admin_menu' ) );
-	add_action( 'admin_menu', 'wp_scoper_admin_menu' );
-	add_action( 'admin_init', 'wp_scoper_admin_init' );
-}
+add_action( 'admin_init', array('OACBaseAdmin', 'init' ), 9 );
+add_action( 'admin_menu', array( 'OACBaseAdmin', 'admin_menu' ), 9 );
+add_action( 'admin_menu', 'wp_scoper_admin_menu' );
+add_action( 'admin_init', 'wp_scoper_admin_init' );
 ?>
